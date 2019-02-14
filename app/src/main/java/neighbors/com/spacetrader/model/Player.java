fb28ba96aac04eaaf485c514c691119cb026402e
JeremyWebb45 <jwebb45@gatech.edu>
2019-02-14 14:43:18 -0500

    Making sure Im committing correctly to Jeremy branch

--- app/src/main/java/neighbors/com/spacetrader/model/Player.java
+++ app/src/main/java/neighbors/com/spacetrader/model/Player.java
@@ -4,4 +4,19 @@ package neighbors.com.spacetrader.model;
  * Player Model to hold data throughout the game
  */
 public class Player {
+    private String name;
+    private int pilotPoints;
+    private int fighterPoints;
+    private int engineerPoints;
+    private int traderPoints;
+
+    public Player() {
+        this("", 0, 0, 0,0);
+    }
+
+    public Player(String pName, int pPoints, int fPoints, int tPoints, int ePoints) {
+        name = pName;
+        pilotPoints = pPoints;
+
+    }
 }
