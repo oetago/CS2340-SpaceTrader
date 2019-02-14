686444dacf0fa5b13e2a8cfb038161a5588f09bb
BRADEN-PC\brade <bchapman34@gatech.edu>
2019-02-14 14:54:26 -0500

    Made the player model

---
+++ app/src/main/java/neighbors/com/spacetrader/model/Difficulty.java
@@ -0,0 +1,9 @@
+package neighbors.com.spacetrader.model;
+
+public enum Difficulty {
+    BEGINNER,
+    EASY,
+    MEDIUM,
+    HARD,
+    IMPOSSIBLE;
+}
--- app/src/main/java/neighbors/com/spacetrader/model/Player.java
+++ app/src/main/java/neighbors/com/spacetrader/model/Player.java
@@ -1,7 +1,58 @@
 package neighbors.com.spacetrader.model;
+import java.util.HashMap;
 
 /**
  * Player Model to hold data throughout the game
  */
 public class Player {
+    private String characterName;
+    private HashMap<SkillCategory, Integer> skills;
+    private Difficulty difficulty;
+    private int credits;
+    private String spaceship;
+    private int availableSkillPoints;
+
+
+    /*
+    All the getter methods for the private fields of the Player class
+     */
+    public String getCharacterName() {
+        return this.characterName;
+    }
+    public HashMap<SkillCategory, Integer> getSkills() {
+        return this.skills;
+    }
+    public Difficulty getDifficulty() {
+        return this.difficulty;
+    }
+    public int getCredits() {
+        return this.credits;
+    }
+    public String getSpaceship() {
+        return this.spaceship;
+    }
+    public int getAvailableSkillPoints() {
+        return this.availableSkillPoints;
+    }
+
+    /*
+    All the setter methods for the private fields of the Player class
+     */
+    public void setCharacterName(String name) {
+        this.characterName = name;
+    }
+    public void assignSkillPoints(SkillCategory category, int numberOfPoints) {
+        skills.put(category,numberOfPoints);
+        this.availableSkillPoints -= numberOfPoints;
+    }
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+    public void setCredits(int credits) {
+        this.credits = credits;
+    }
+    public void setSpaceship(String spaceship) {
+        this.spaceship = spaceship;
+    }
+
 }
