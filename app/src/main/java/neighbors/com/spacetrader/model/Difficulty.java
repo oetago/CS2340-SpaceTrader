321f01cab6a7c6976d0c3f64784b3a51c22f9486
alonsom1117 <46792063+alonsom1117@users.noreply.github.com>
2019-02-14 15:02:49 -0500

    Added Spinner for Difficulty
    * Added Spinner for Difficulty
    
    created enum class for difficulties
    added the spinner object to the config
    added the spinner to onCreate method with Array Adapter
    
    * Move difficulty to model and auto launch config activity

--- app/src/main/java/neighbors/com/spacetrader/model/Difficulty.java
+++ app/src/main/java/neighbors/com/spacetrader/model/Difficulty.java
@@ -1,9 +1,17 @@
 package neighbors.com.spacetrader.model;
 
 public enum Difficulty {
-    BEGINNER,
-    EASY,
-    MEDIUM,
-    HARD,
-    IMPOSSIBLE;
+    BEGINNER ("Beginner"),
+    EASY ("Easy"),
+    MEDIUM ("Medium"),
+    HARD ("Hard"),
+    IMPOSSIBLE ("Impossible");
+
+    private final String name;
+
+    Difficulty (String pname) {
+        name = pname;
+    }
+
+    public String getName() { return name; }
 }
--- app/src/main/java/neighbors/com/spacetrader/model/Player.java
+++ app/src/main/java/neighbors/com/spacetrader/model/Player.java
@@ -1,10 +1,17 @@
 package neighbors.com.spacetrader.model;
 import java.util.HashMap;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Player Model to hold data throughout the game
  */
 public class Player {
+    private Difficulty difficulty;
+
+    public static List<String> legalDifficulty = Arrays.asList
+            ("Beginner", "Easy", "Medium", "Hard", "Impossible");
     private String characterName;
     private HashMap<SkillCategory, Integer> skills;
     private Difficulty difficulty;
