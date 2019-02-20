1a1d7e87fe93d2b10045e17ac7a032b65434124f
Ryan Miles <ryanm1114@gmail.com>
2019-02-20 17:45:32 -0500

    Code clean up! Use view model to store data!

--- app/src/main/java/neighbors/com/spacetrader/model/Difficulty.java
+++ app/src/main/java/neighbors/com/spacetrader/model/Difficulty.java
@@ -1,5 +1,11 @@
 package neighbors.com.spacetrader.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Game difficulty enum
+ */
 public enum Difficulty {
     BEGINNER("Beginner"),
     EASY("Easy"),
@@ -9,8 +15,21 @@ public enum Difficulty {
 
     private final String name;
 
-    Difficulty(String pname) {
-        name = pname;
+    Difficulty(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Get List of difficulties for adapters
+     *
+     * @return a list of the difficulties
+     */
+    public static List<String> stringValues() {
+        ArrayList<String> values = new ArrayList<>();
+        for (Difficulty value : values()) {
+            values.add(value.name);
+        }
+        return values;
     }
 
     public String getName() {
--- app/src/main/java/neighbors/com/spacetrader/model/SkillCategory.java
+++
@@ -1,8 +0,0 @@
-package neighbors.com.spacetrader.model;
-
-public enum SkillCategory {
-    PILOT,
-    TRADER,
-    FIGHTER,
-    ENGINEER;
-}
